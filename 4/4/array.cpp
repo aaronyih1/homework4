6317197f8e6c2ed539031a6bf20f4f04a62fa385
Aaron Yih <aaron.yih@ucla.edu>
2015-05-04 19:05:28 -0700

    hey there

--- 4/4/array.cpp
+++ 4/4/array.cpp
@@ -14,6 +14,10 @@ using namespace std;
 
 int enumerate(const string a[], int n, string target){
     int counter=0;
+    if(n<0)
+    {
+        return(-1);
+    }
     for(int i=0; i<n; i++)
     {
         if(a[i]==target)
@@ -24,9 +28,72 @@ int enumerate(const string a[], int n, string target){
     return(counter);
 }
 
+int locate(const string a[], int n, string target){
+    if(n<0)
+    {
+        return(-1);
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(a[i] == target){
+            return(i);
+        }
+    }
+    return(-1);
+}
 
+bool locateSequence(const string a[], int n, string target, int& begin, int& end){
+    int counter=0;
+    if(n<0)
+    {
+        return(false);
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(a[i]==target)
+        {
+            begin=i;
+            while(a[i]==target){
+                counter++;
+                i++;
+            }
+            end=begin+counter-1;
+            return(true);
+        }
+    }
+    return(false);
+}
+
+int locationOfMin(const string a[], int n){
+    string min=a[0];
+    int position=0;
+    for(int i=1; i<n;i++)
+    {
+        if(a[i]<min)
+        {
+            min=a[i];
+            position=i;
+        }
+    }
+    return(position);
+}
+
+int moveToEnd(string a[], int n, int pos){
+    int temp = pos;
+    for(int i=pos+1; i<n-1; i++)
+    {
+        a[i-1]=a[i];
+    }
+    a[n-1]=a[temp];
+    for(int j=0; j<n; j++){
+        cerr<<a[j]<<endl;
+    }
+    return(pos);
+    
+}
 
 int main() {
-    string people[8]={"samwell", "jon", "margaery", "daenerys", "tyrion", "tyrion", "tyrion"};
-    cerr<< enumerate(people,8,"tyrion")<< endl;
+    string people[5] = { "samwell", "jon", "margaery", "daenerys", "tyrion" };
+    int j = moveToEnd(people, 5, 1);
+    cerr<<j<<endl;
 }
